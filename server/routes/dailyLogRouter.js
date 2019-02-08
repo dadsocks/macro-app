943865d4b69f5f1c94320e2b1dfe70342a391c2e
Bill Taggart <taggarw@gmail.com>
2019-02-07 22:00:23 -0700

    Add POST /dailyLog , GET /dailyLog/:id, and DELETE /dailyLog/:id to dailyLogRouter

---
+++ server/routes/dailyLogRouter.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const router = express.Router();
+const Joi = require('joi');
+const { ObjectID } = require('mongodb');
+const { dailyLogSchema } = require('./../utils/joiSchemas');
+const { DailyLog } = require('./../models/dailyLog');
+
+router.get('/', (req, res) => {
+  console.log(req.query.date);
+
+  const date = new Date(req.query.date).toString();
+
+  if(Object.keys(req.query).length === 0) {
+    return DailyLog.find().then((dailyLogs) => {
+      res.send({dailyLogs});
+    }, (e) => {
+      res.status(400).send(e);
+    });
+  }
+
+  DailyLog.find({date}).then((dailyLogs) => {
+    res.send({dailyLogs});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+router.get('/:id', (req, res) => {
+
+  const id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send('ID is not valid');
+  }
+
+  DailyLog.findOne({_id: id}).then((dailyLog) => {
+    if(!dailyLog) {
+      return res.status(404).send('Daily log was not found');
+    }
+
+    res.send({dailyLog});
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
+router.post('/', (req, res) => {
+
+  const date = new Date(req.body.date).toString();
+
+  const dailyLogReq = {
+      date: date,
+      weight: req.body.weight,
+      waistMeasurement: req.body.waistMeasurement,
+      bodyFatPercentage: req.body.bodyFatPercentage,
+      sleep: req.body.sleep,
+      water: req.body.water,
+      dayType: req.body.dayType,
+      carbohydrates: req.body.carbohydrates,
+      fat: req.body.fat,
+      protein: req.body.protein
+  };
+
+  const result = Joi.validate(dailyLogReq, dailyLogSchema, {abortEarly: false});
+
+  if(result.error) {
+    return res.status(400).send(result.error);
+  }
+
+  const dailyLog = new DailyLog(dailyLogReq);
+
+  dailyLog.save().then((log) => {
+    res.send(log);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+router.delete('/:id', (req,res) => {
+
+  const id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send('ID is not valid');
+  }
+
+  DailyLog.findOneAndDelete({_id: id}).then((dailyLog) => {
+    if(!dailyLog) {
+      return res.status(404).send('Daily log was not found');
+    }
+
+    res.send({dailyLog});
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,19 +3,19 @@ require('./config/config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
-
-const {ObjectID} = require('mongodb');
-
 const Joi = require('joi');
+const { ObjectID } = require('mongodb');
 const { calculateMacros } = require('./utils/macroUtils');
 const { macroSchema, dailyLogSchema } = require('./utils/joiSchemas');
 const { DailyLog } = require('./models/dailyLog');
-
 const { mongoose } = require('./db/database');
-
 const app = express();
 const port = process.env.PORT;
+const dailyLogRouter = require('./routes/dailyLogRouter');
+
 app.use(bodyParser.json());
+
+app.use('/dailyLog', dailyLogRouter);
 
 app.get('/macros', (req,res) => {
   const result = Joi.validate(req.body, macroSchema, {abortEarly: false});
@@ -26,94 +26,6 @@ app.get('/macros', (req,res) => {
 
   return res.send(calculateMacros(req.body));
 
-});
-
-app.post('/dailyLog', (req, res) => {
-
-  const date = new Date(req.body.date).toString();
-  const dailyLogReq = {
-      date: date,
-      weight: req.body.weight,
-      waistMeasurement: req.body.waistMeasurement,
-      bodyFatPercentage: req.body.bodyFatPercentage,
-      sleep: req.body.sleep,
-      water: req.body.water,
-      dayType: req.body.dayType,
-      carbohydrates: req.body.carbohydrates,
-      fat: req.body.fat,
-      protein: req.body.protein
-  };
-  const result = Joi.validate(dailyLogReq, dailyLogSchema, {abortEarly: false});
-
-  if(result.error) {
-    return res.status(400).send(result.error);
-  }
-
-  const dailyLog = new DailyLog(dailyLogReq);
-
-  dailyLog.save().then((log) => {
-    res.send(log);
-  }, (e) => {
-    res.status(400).send(e);
-  });
-});
-
-app.get('/dailyLog', (req, res) => {
-  console.log(req.query.date);
-
-  const date = new Date(req.query.date).toString();
-
-  if(Object.keys(req.query).length === 0) {
-    return DailyLog.find().then((dailyLogs) => {
-      res.send({dailyLogs});
-    }, (e) => {
-      res.status(400).send(e);
-    });
-  }
-
-  DailyLog.find({date}).then((dailyLogs) => {
-    res.send({dailyLogs});
-  }, (e) => {
-    res.status(400).send(e);
-  });
-});
-
-app.get('/dailyLog/:id', (req, res) => {
-
-  const id = req.params.id;
-
-  if(!ObjectID.isValid(id)) {
-    return res.status(404).send('ID is not valid');
-  }
-
-  DailyLog.findOne({_id: id}).then((dailyLog) => {
-    if(!dailyLog) {
-      return res.status(404).send('Daily log was not found');
-    }
-
-    res.send({dailyLog});
-  }).catch((e) => {
-    res.status(400).send(e);
-  });
-});
-
-app.delete('/dailyLog/:id', (req, res) => {
-
-  const id = req.params.id;
-
-  if(!ObjectID.isValid(id)) {
-    return res.status(404).send('ID is not valid');
-  }
-
-  DailyLog.findOneAndDelete({_id: id}).then((dailyLog) => {
-    if(!dailyLog) {
-      return res.status(404).send('Daily log was not found');
-    }
-
-    res.send({dailyLog});
-  }).catch((e) => {
-    res.status(400).send(e);
-  });
 });
 
 app.patch('/dailyLog/:id', (req, res) => {
